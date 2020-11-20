dfedfaa9fcd0097fe2dbe07f9b0c7d26c2604d80
Marek Knápek <knapek.mar@gmail.com>
2020-11-20 03:03:00 +0000

    Enable libFuzzer.

---
+++ bag_to_pcap/bag_to_pcap/src/bag_to_pcap_fuzz.cpp
@@ -0,0 +1,24 @@
+#include "bag.h"
+
+#include <cstdint>
+
+
+static unsigned g_do_not_optimise;
+
+
+extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* const data, std::size_t const size)
+{
+	mk::bag::records_t records;
+	bool const parsed = mk::bag::parse(data, size, &records);
+	g_do_not_optimise += parsed ? 1 : 0;
+	g_do_not_optimise += static_cast<int>(records.size());
+	return 0;
+}
+
+
+#include "bag.cpp"
+#include "utils.cpp"
+
+
+// clang++-10 -g -O1 -std=c++20 -DFUZZING -DNDEBUG -fsanitize=fuzzer,address bag_to_pcap/bag_to_pcap/src/bag_to_pcap_fuzz.cpp -o bag_to_pcap_fuzz.bin
+// ./bag_to_pcap_fuzz.bin fuzz_corpus -jobs=7 -workers=7
--- bag_to_pcap/bag_to_pcap/src/utils.cpp
+++ bag_to_pcap/bag_to_pcap/src/utils.cpp
@@ -7,6 +7,7 @@
 
 void mk::check_ret_failed(char const* const& file, int const& line, char const* const& expr)
 {
+	#ifndef FUZZING
 	std::array<char, 64 * 1024> buff;
 	int const formatted = std::snprintf(buff.data(), buff.size(), "CHECK_RET failed in file '%s' on line %d with '%s'.", file, line, expr);
 	assert(formatted >= 0 && formatted < static_cast<int>(buff.size()));
@@ -14,4 +15,5 @@ void mk::check_ret_failed(char const* const& file, int const& line, char const* 
 	int const printed = std::puts(buff.data());
 	assert(printed != EOF && printed >= 0);
 	(void)printed;
+	#endif
 }
