ad01bd24f8c39d65a3b244273d735d4489572723
Marek Knápek <knapek.mar@gmail.com>
2020-12-29 03:03:00 +0000

    Add CHECK_RET_F macro.

--- bag_to_pcap/bag_to_pcap/src/utils.h
+++ bag_to_pcap/bag_to_pcap/src/utils.h
@@ -3,6 +3,7 @@
 
 #define CHECK_RET(X, R) do{ if(X){}else{ mk::check_ret_failed(__FILE__, static_cast<int>(__LINE__), #X); return R; } }while(false)
 #define CHECK_RET_V(X) do{ if(X){}else{ mk::check_ret_failed(__FILE__, static_cast<int>(__LINE__), #X); return; } }while(false)
+#define CHECK_RET_F(X) do{ if(X){}else{ mk::check_ret_failed(__FILE__, static_cast<int>(__LINE__), #X); return false; } }while(false)
 #define CHECK_RET_CRASH(X) do{ if(X){}else{ mk::check_ret_failed(__FILE__, static_cast<int>(__LINE__), #X); int volatile* volatile ptr = nullptr; *ptr = 0; } }while(false)
 
 
